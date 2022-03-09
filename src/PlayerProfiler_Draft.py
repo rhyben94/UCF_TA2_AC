da2b25ba50c0447f7655f55d0bb37293b5e411e8
Rhyse <54722928+rhyben94@users.noreply.github.com>
2022-03-08 19:11:22 -0500

    Default values
    Modifying default values for survey responses to account for missing data

--- src/PlayerProfiler_Draft.py
+++ src/PlayerProfiler_Draft.py
@@ -23,63 +23,63 @@ Competency_Score = 200
 
 ##{"ImportId":"QID13_1"}
 ##b0_Q10_1
-SBSOD_1 = 2
+SBSOD_1 = 5.502857143
 
 ##{"ImportId":"QID13_2"}
 ##b0_Q10_2
-SBSOD_2 = 4
+SBSOD_2 = 3.44
 
 ##{"ImportId":"QID13_3"}
 ##b0_Q10_3
-SBSOD_3 = 1
+SBSOD_3 = 5.098837209
 
 ##{"ImportId":"QID13_4"}
 ##b0_Q10_4
-SBSOD_4 = 3
+SBSOD_4 = 5.402298851
 
 ##{"ImportId":"QID13_5"}
 ##b0_Q10_5
-SBSOD_5 = 3
+SBSOD_5 = 4.165714286
 
 ##{"ImportId":"QID13_6"}
 ##b0_Q10_6
-SBSOD_6 = 4
+SBSOD_6 = 3.782857143
 
 ##{"ImportId":"QID13_7"}
 ##b0_Q10_7
-SBSOD_7 = 6
+SBSOD_7 = 4.908571429
 
 ##{"ImportId":"QID13_8"}
 ##b0_Q10_8
-SBSOD_8 = 7
+SBSOD_8 = 2.534482759
 
 ##{"ImportId":"QID13_9"}
 ##b0_Q10_9
-SBSOD_9 = 5
+SBSOD_9 = 5.218390805
 
 ##{"ImportId":"QID13_10"}
 ##b0_Q10_10
-SBSOD_10 = 4
+SBSOD_10 = 3.508571429
 
 ##{"ImportId":"QID13_11"}
 ##b0_Q10_11
-SBSOD_11 = 2
+SBSOD_11 = 3.281609195
 
 ##{"ImportId":"QID13_12"}
 ##b0_Q10_12
-SBSOD_12 = 6
+SBSOD_12 = 2.350574713
 
 ##{"ImportId":"QID13_13"}
 ##b0_Q10_13
-SBSOD_13 = 7
+SBSOD_13 = 3.482758621
 
 ##{"ImportId":"QID13_14"}
 ##b0_Q10_14
-SBSOD_14 = 1
+SBSOD_14 = 4.457142857
 
 ##{"ImportId":"QID13_15"}
 ##b0_Q10_15
-SBSOD_15 = 5
+SBSOD_15 = 2.754285714
 
 
 ## The video game experience measure scores need to be standardized for calculation
@@ -87,22 +87,22 @@ SBSOD_15 = 5
 ##{"ImportId":"QID867_1"}
 ##b0_Q20_1
 ##Please indicate how many years of experience you have with the following: - Years using a computer for any purpose
-VGEM_1 = 12*5
+VGEM_1 = 14.09
 
 ##{"ImportId":"QID867_2"}
 ##b0_Q20_2
 ##Please indicate how many years of experience you have with the following: - Years using a computer to play video games
-VGEM_2 = 12*5
+VGEM_2 = 11.58
 
 ##{"ImportId":"QID867_4"}
 ##b0_Q20_4
 ##Please indicate how many years of experience you have with the following: - Years playing Minecraft
-VGEM_3 = 12*5
+VGEM_3 = 8.08
 
 ##{"ImportId":"QID868_1"}
 ##b0_Q19_1
 ##For the following, please indicate how regularly you: - Use a computer
-VGEM_4 = 3
+VGEM_4 = 7
 if VGEM_4 == 1:
     VGEM_4 = 0
 elif VGEM_4 == 2:
@@ -133,7 +133,7 @@ def get_vgem_4(qid_val):
 ##{"ImportId":"QID868_3"}
 ##b0_Q19_3
 ##For the following, please indicate how regularly you: - Use a computer to play video games
-VGEM_5 = 2
+VGEM_5 = 7
 if VGEM_5 == 1:
     VGEM_5 = 0
 elif VGEM_5 == 2:
@@ -164,7 +164,7 @@ def get_vgem_5(qid_val):
 ##{"ImportId":"QID868_5"}
 ##b0_Q19_5
 ##For the following, please indicate how regularly you: - Play video games which require participation in a team
-VGEM_6 = 4
+VGEM_6 = 7
 if VGEM_6 == 1:
     VGEM_6 = 0
 elif VGEM_6 == 2:
@@ -195,7 +195,7 @@ def get_vgem_6(qid_val):
 ##{"ImportId":"QID868_6"}
 ##b0_Q19_6
 ##For the following, please indicate how regularly you: - Play Minecraft
-VGEM_7 = 2
+VGEM_7 = 6
 if VGEM_7 == 1:
     VGEM_7 = 0
 elif VGEM_7 == 2:
@@ -225,193 +225,193 @@ def get_vgem_7(qid_val):
 ##{"ImportId":"QID872_1"}
 ##b0_Q15_1
 ##Learning the layout of a new virtual environment
-VGEM_8 = 80
+VGEM_8 = 74.29714286
 
 ##{"ImportId":"QID872_2"}
 ##b0_Q15_2
 ##Communicating your current location in a virtual environment to members of a team
-VGEM_9 = 30
+VGEM_9 = 70.40571429
 
 ##{"ImportId":"QID872_3"}
 ##b0_Q15_3
 ##Coordinating with teammates to optimize tasks
-VGEM_10 = 40
+VGEM_10 = 76.94285714
 
 ##{"ImportId":"QID872_4"}
 ##b0_Q15_4
 ##Maintaining an awareness of game/task parameters (e.g., time limits, point goals, etc)
-VGEM_11 = 60
+VGEM_11 = 80.41714286
 
 ##{"ImportId":"QID872_5"}
 ##b0_Q15_5
 ##Learning the purposes of novel items, tools, or objects
-VGEM_12 = 20
+VGEM_12 = 79.94857143
 
 ##{"ImportId":"QID872_6"}
 ##b0_Q15_6
 ##Remembering which places you have visited  in a virtual environment
-VGEM_13 = 10
+VGEM_13 = 77.48
 
 ##{"ImportId":"QID872_7"}
 ##b0_Q15_7
 ##Controlling the movement of an avatar using the W, A, S, and D keys + mouse control
-VGEM_14 = 90
+VGEM_14 = 92.85142857
 
 ##{"ImportId":"QID872_8"}
 ##b0_Q15_8
 ##Keeping track of where you are in a virtual environment
-VGEM_15 = 40
+VGEM_15 = 80.44571429
 
 ##{"ImportId":"QID830_1"}
 ##b0_Q21_1
 ##I preferred to work in those groups rather than working alone
-PsychologicalCollectivism_1 = 4
+PsychologicalCollectivism_1 = 3.508571429
 
 ##{"ImportId":"QID830_2"}
 ##b0_Q21_2
 ##Working in those groups was better than working alone.
-PsychologicalCollectivism_2 = 6
+PsychologicalCollectivism_2 = 3.622857143
 
 ##{"ImportId":"QID830_3"}
 ##b0_Q21_3
 ##I wanted to work with those groups as opposed to working alone.
-PsychologicalCollectivism_3 = 3
+PsychologicalCollectivism_3 = 3.451428571
 
 ##{"ImportId":"QID830_4"}
 ##b0_Q21_4
 ##I felt comfortable counting on group members to do their part.
-PsychologicalCollectivism_4 = 7
+PsychologicalCollectivism_4 = 3.497142857
 
 ##{"ImportId":"QID830_5"}
 ##b0_Q21_5
 ##I was not bothered by the need to rely on group members.
-PsychologicalCollectivism_5 = 1
+PsychologicalCollectivism_5 = 3.551724138
 
 ##{"ImportId":"QID830_6"}
 ##b0_Q21_6
 ##I felt comfortable tasking group members to handle their tasks.
-PsychologicalCollectivism_6 = 2
+PsychologicalCollectivism_6 = 3.88
 
 ##{"ImportId":"QID830_7"}
 ##b0_Q21_7
 ##The health of those groups was important to me.
-PsychologicalCollectivism_7 = 5
+PsychologicalCollectivism_7 = 4.28
 
 ##{"ImportId":"QID830_8"}
 ##b0_Q21_8
 ##I cared about the well-being of those groups.
-PsychologicalCollectivism_8 = 4
+PsychologicalCollectivism_8 = 4.274285714
 
 ##{"ImportId":"QID830_9"}
 ##b0_Q21_9
 ##I was concerned about the needs of those groups.
-PsychologicalCollectivism_9 = 6
+PsychologicalCollectivism_9 = 4.195402299
 
 ##{"ImportId":"QID830_10"}
 ##b0_Q21_10
 ##I followed the norms of those groups.
-PsychologicalCollectivism_10 = 2
+PsychologicalCollectivism_10 = 4.017142857
 
 ##{"ImportId":"QID830_11"}
 ##b0_Q21_11
 ##I followed the procedures used by those groups.
-PsychologicalCollectivism_11 = 4
+PsychologicalCollectivism_11 = 4.354285714
 
 ##{"ImportId":"QID830_12"}
 ##b0_Q21_12
 ##I accepted the rules of those groups.
-PsychologicalCollectivism_12 = 2
+PsychologicalCollectivism_12 = 4.494252874
 
 ##{"ImportId":"QID830_13"}
 ##b0_Q21_13
 ##I cared more about the goals of those groups than my own goals.
-PsychologicalCollectivism_13 = 4
+PsychologicalCollectivism_13 = 3.542857143
 
 ##{"ImportId":"QID830_14"}
 ##b0_Q21_14
 ##I emphasized the goals of those groups more than my individual goals.
-PsychologicalCollectivism_14 = 5
+PsychologicalCollectivism_14 = 3.634285714
 
 ##{"ImportId":"QID830_15"}
 ##b0_Q21_15
 ##Group goals were more important to me than my personal goals.
-PsychologicalCollectivism_15 = 6
+PsychologicalCollectivism_15 = 3.417142857
 
 
 ##{"ImportId":"QID832_9"}
 ##b0_Q22_1
 ##I have no problems talking in front of a group.
-SociableDominance_1 = 1
+SociableDominance_1 = 3.925714286
 
 ##{"ImportId":"QID832_16"}
 ##b0_Q22_2
 ##At school I found it easy to talk in front of the class
-SociableDominance_2 = 4
+SociableDominance_2 = 3.417142857
 
 ##{"ImportId":"QID832_17"}
 ##b0_Q22_3
 ##No doubt I’ll make a good leader.
-SociableDominance_3 = 3
+SociableDominance_3 = 3.534482759
 
 ##{"ImportId":"QID832_18"}
 ##b0_Q22_4
 ##I certainly have self-confidence.
-SociableDominance_4 = 5
+SociableDominance_4 = 3.811428571
 
 ##{"ImportId":"QID832_19"}
 ##b0_Q22_5
 ##For me it is not hard to start a conversation in a group.
-SociableDominance_5 = 6
+SociableDominance_5 = 3.337142857
 
 ##{"ImportId":"QID832_20"}
 ##b0_Q22_6
 ##I am not shy with strangers.
-SociableDominance_6 = 3
+SociableDominance_6 = 3.24
 
 ##{"ImportId":"QID832_21"}
 ##b0_Q22_7
 ##I like taking responsibility.
-SociableDominance_7 = 4
+SociableDominance_7 = 3.925714286
 
 ##{"ImportId":"QID832_22"}
 ##b0_Q22_8
 ##People turn to me for decisions
-SociableDominance_8 = 5
+SociableDominance_8 = 3.683908046
 
 ##{"ImportId":"QID832_23"}
 ##b0_Q22_9
 ##I can look everybody in the eye, and lie with a straight face
-SociableDominance_9 = 1
+SociableDominance_9 = 3.068965517
 
 ##{"ImportId":"QID832_24"}
 ##b0_Q22_10
 ##I can lie without anybody noticing it
-SociableDominance_10 = 2
+SociableDominance_10 = 3.108571429
 
 ##{"ImportId":"QID832_25"}
 ##b0_Q22_11
 ##I find it important to get my way.
-SociableDominance_11 = 4
+SociableDominance_11 = 2.645714286
 
 ##{"ImportId":"QID832_26"}
 ##b0_Q22_12
 ##I find it important to get my way, even if this causes a row
-SociableDominance_12 = 5
+SociableDominance_12 = 1.885714286
 
 ##{"ImportId":"QID832_27"}
 ##b0_Q22_13
 ##I quickly feel aggressive with people
-SociableDominance_13 = 6
+SociableDominance_13 = 1.6
 
 ##{"ImportId":"QID832_28"}
 ##b0_Q22_14
 ##I make smart, sarcastic remarks if people deserve it
-SociableDominance_14 = 4
+SociableDominance_14 = 2.794285714
 
 ##{"ImportId":"QID832_29"}
 ##b0_Q22_15
 ##I’d rather be disliked (for being unkind) and that people look down on me (for not achieving my aims)
-SociableDominance_15 = 7
+SociableDominance_15 = 1.874285714
 
 
 
@@ -425,7 +425,7 @@ RMET_2 = 2
 
 ##{"ImportId":"QID755"}
 ##b0_Q30
-RMET_3 = 4
+RMET_3 = 3
 
 ##{"ImportId":"QID757"}
 ##b0_Q32
@@ -433,19 +433,19 @@ RMET_4 = 2
 
 ##{"ImportId":"QID759"}
 ##b0_Q34
-RMET_5 = 1
+RMET_5 = 2
 
 ##{"ImportId":"QID761"}
 ##b0_Q36
-RMET_6 = 3
+RMET_6 = 2
 
 ##{"ImportId":"QID763"}
 ##b0_Q38
-RMET_7 = 4
+RMET_7 = 2
 
 ##{"ImportId":"QID765"}
 ##b0_Q40
-RMET_8 = 3
+RMET_8 = 1
 
 ##{"ImportId":"QID767"}
 ##b0_Q42
@@ -453,19 +453,19 @@ RMET_9 = 4
 
 ##{"ImportId":"QID769"}
 ##b0_Q44
-RMET_10 = 2
+RMET_10 = 1
 
 ##{"ImportId":"QID771"}
 ##b0_Q46
-RMET_11 = 3
+RMET_11 = 2
 
 ##{"ImportId":"QID773"}
 ##b0_Q48
-RMET_12 = 4
+RMET_12 = 3
 
 ##{"ImportId":"QID775"}
 ##b0_Q50
-RMET_13 = 1
+RMET_13 = 2
 
 ##{"ImportId":"QID777"}
 ##b0_Q52
@@ -473,23 +473,23 @@ RMET_14 = 1
 
 ##{"ImportId":"QID779"}
 ##b0_Q54
-RMET_15 = 2
+RMET_15 = 1
 
 ##{"ImportId":"QID781"}
 ##b0_Q56
-RMET_16 = 3
+RMET_16 = 2
 
 ##{"ImportId":"QID783"}
 ##b0_Q58
-RMET_17 = 4
+RMET_17 = 2
 
 ##{"ImportId":"QID785"}
 ##b0_Q60
-RMET_18 = 3
+RMET_18 = 1
 
 ##{"ImportId":"QID787"}
 ##b0_Q62
-RMET_19 = 1
+RMET_19 = 2
 
 ##{"ImportId":"QID789"}
 ##b0_Q64
@@ -497,23 +497,23 @@ RMET_20 = 2
 
 ##{"ImportId":"QID791"}
 ##b0_Q66
-RMET_21 = 3
+RMET_21 = 2
 
 ##{"ImportId":"QID793"}
 ##b0_Q68
-RMET_22 = 4
+RMET_22 = 1
 
 ##{"ImportId":"QID795"}
 ##b0_Q70
-RMET_23 = 3
+RMET_23 = 4
 
 ##{"ImportId":"QID797"}
 ##b0_Q72
-RMET_24 = 2
+RMET_24 = 1
 
 ##{"ImportId":"QID799"}
 ##b0_Q74
-RMET_25 = 1
+RMET_25 = 4
 
 ##{"ImportId":"QID801"}
 ##b0_Q76
@@ -537,15 +537,15 @@ RMET_30 = 2
 
 ##{"ImportId":"QID811"}
 ##b0_Q86
-RMET_31 = 3
+RMET_31 = 2
 
 ##{"ImportId":"QID813"}
 ##b0_Q88
-RMET_32 = 4
+RMET_32 = 1
 
 ##{"ImportId":"QID815"}
 ##b0_Q90
-RMET_33 = 3
+RMET_33 = 4
 
 ##{"ImportId":"QID817"}
 ##b0_Q92
@@ -553,11 +553,11 @@ RMET_34 = 3
 
 ##{"ImportId":"QID819"}
 ##b0_Q94
-RMET_35 = 1
+RMET_35 = 2
 
 ##{"ImportId":"QID821"}
 ##b0_Q96
-RMET_36 = 1
+RMET_36 = 3
 
 ## Measure score calculations
 
