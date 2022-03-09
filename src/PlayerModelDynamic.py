23e6d44500a95d05938f32f20856487d169a0197
Prakash Manghwani <prakash@dollabs.com>
2022-03-09 13:32:57 -0500

    Added parameters

---
+++ src/PlayerModelDynamic.py
@@ -0,0 +1,80 @@
+# Ref https://docs.google.com/presentation/d/1Z1QMkGs2D6fZSxsZO9CM2bFzGYzZaVbjTieoUenhQho/edit#slide=id.g117addec6ce_0_45
+
+TaskPotential_LowModifier = 0
+TaskPotential_HighModifier = 0
+
+Medic_TaskPotential_RecategorizationThresholds_High_List = [4, 4, 4, 5, 5]
+Medic_TaskPotential_RecategorizationThresholds_Low_List = [-1, 0, 1, 1, 2]
+
+Engineer_TaskPotential_RecategorizationThresholds_High_List = [4, 4, 4, 5, 5]
+Engineer_TaskPotential_RecategorizationThresholds_Low_List = [-1, 0, 1, 1, 2]
+
+Transporter_TaskPotential_RecategorizationThresholds_High_List = [4, 4, 4, 5, 5]
+Transporter_TaskPotential_RecategorizationThresholds_Low_List = [-1, 0, 1, 1, 2]
+
+Medic_TaskPotential_RealTime_Factors_Coefficient = 1
+Engineer_TaskPotential_RealTime_Factors_Coefficient = 1
+Transporter_TaskPotential_RealTime_Factors_Coefficient = 1
+
+Medic_FactorCoefficient_1 = 5
+Medic_FactorCoefficient_2 = 10
+Medic_FactorCoefficient_3 = 1
+Medic_FactorCoefficient_4 = 2
+Medic_FactorCoefficient_5 = 10
+
+Engineer_FactorCoefficient_1 = 10
+Engineer_FactorCoefficient_2 = 3
+Engineer_FactorCoefficient_3 = 6
+Engineer_FactorCoefficient_4 = 2
+Engineer_FactorCoefficient_5 = 6
+
+Transporter_FactorCoefficient_1 = 5
+Transporter_FactorCoefficient_2 = 10
+Transporter_FactorCoefficient_3 = 3
+Transporter_FactorCoefficient_4 = 2
+Transporter_FactorCoefficient_5 = 2
+
+Medic_TriageSuccessful_UpperThreshold_List = [2, 3, 4, 5, 5]
+Medic_TriageSuccessful_LowerThreshold_List = [0, 1, 1, 1, 1]
+
+Medic_TriageSuccessful_to_MovementRatio_UpperThreshold_List = [2 / 300, 2 / 300, 3 / 300, 3 / 300, 4 / 300]
+Medic_TriageSuccessful_to_MovementRatio_LowerThreshold_List = [2 / 600, 2 / 600, 3 / 700, 3 / 700, 4 / 800]
+
+Medic_Transport_Distance_Average_UpperThreshold_List = [.5, .5, .5, .5, .5]
+Medic_Transport_Distance_Average_LowerThreshold_List = [.1, .1, .1, .1, .1]
+
+Medic_TriageSuccessful_to_Transports_ratio_UpperThreshold_List = [1.75, 1.75, 2, 2, 2]
+Medic_TriageSuccessful_to_Transports_ratio_LowerThreshold_List = [.75, .75, .75, .75, .75]
+
+Medic_Transports_Completed_Count_UpperThreshold_List = [2, 2, 2, 2, 2]
+Medic_Transports_Completed_Count_LowerThreshold_List = [0, 0, 0, 0, 0]
+
+Engineer_RubbleDestroyed_Count_UpperThreshold_List = [4, 8, 8, 8, 8]
+Engineer_RubbleDestroyed_Count_LowerThreshold_List = [0, 2, 2, 2, 2]
+
+Engineer_RubbleDestroyed_to_MovementRatio_UpperThreshold_List = [6 / 300, 8 / 300, 8 / 300, 8 / 300, 8 / 300]
+Engineer_RubbleDestroyedMovementRatio_LowerThreshold_List = [6 / 400, 8 / 600, 8 / 600, 8 / 300, 8 / 600]
+
+Engineer_Transport_Distance_Average_UpperThreshold_List = [.5, .5, .5, .5, .5]
+Engineer_Transport_Distance_Average_LowerThreshold_List = [.1, .1, .1, .1, .1]
+
+Engineer_Transports_Completed_Count_UpperThreshold_List = [3, 3, 3, 3, 3]
+Engineer_Transports_Completed_Count_LowerThreshold_List = [0, 1, 1, 1, 1]
+
+Engineer_Rubble_to_Transports_ratio_UpperThreshold_List = [3, 3, 4, 4, 4]
+Engineer_Rubble_to_Transports_ratio_LowerThreshold_List = [1, 1, .75, .75, .75]
+
+Transporter_Evacuations_Regular_Count_UpperThreshold_List = []
+Transporter_Evacuations_Regular_Count_LowerThreshold_List = []
+
+Transporter_Evacuations_Regular_Count_UpperThreshold_List = []
+Transporter_Evacuations_Regular_Count_LowerThreshold_List = []
+
+Transporter_Transport_Distance_Average_UpperThreshold_List = []
+Transporter_Transport_Distance_Average_LowerThreshold_List = []
+
+Transporter_Transport_to_Evacuation_ratio_UpperThreshold_List = []
+Transporter_Transports_Completed_Count_UpperThreshold_List = []
+
+Transporter_Transport_to_Evacuation_ratio_LowerThreshold_List = []
+Transporter_Transports_Completed_Count_LowerThreshold_List = []
